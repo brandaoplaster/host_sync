a91c248c2293c9d60b682fb6841c10b3d136c0c2
brandaoplaster <brandaoplaster@gmail.com>
2024-09-02 22:23:17 -0300

    fix: Fixing Gettext

--- lib/host_sync_web/gettext.ex
+++ lib/host_sync_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule HostSyncWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :host_sync
+  use Gettext.Backend, otp_app: :host_sync
 end
