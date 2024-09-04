35bf632fe48fd81a2ef60ccdfc2a06729242b170
brandaoplaster <brandaoplaster@gmail.com>
2024-09-03 22:41:18 -0300

    feat: Configure dev environment

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :host_sync, HostSync.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "host_sync_db",
   database: "host_sync_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -19,7 +19,7 @@ config :host_sync, HostSync.Repo,
 config :host_sync, HostSyncWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
