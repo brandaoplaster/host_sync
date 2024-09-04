6158d2dd9569b0ddcc36a8d835364cad44538abc
brandaoplaster <brandaoplaster@gmail.com>
2024-09-03 22:36:48 -0300

    feat: Adding user schema

---
+++ lib/host_sync/accounts/user.ex
@@ -0,0 +1,34 @@
+defmodule HostSync.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  @required_params ~w(name email password)a
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
