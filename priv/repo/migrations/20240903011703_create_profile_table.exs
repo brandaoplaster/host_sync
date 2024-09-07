0dfca18e7619bbc00aff7679de089bfc1402bcb1
brandaoplaster <brandaoplaster@gmail.com>
2024-09-07 19:15:55 -0300

    feat: Updated migration profiles

--- priv/repo/migrations/20240903011703_create_profile_table.exs
+++ priv/repo/migrations/20240903011703_create_profile_table.exs
@@ -4,9 +4,16 @@ defmodule HostSync.Repo.Migrations.CreateProfileTable do
   def change do
     create table(:profiles, primary_key: false) do
       add :id, :uuid, primary_key: true
+      add :max_price, :integer
+      add :guest_occupation, :string
+      add :is_smoker, :boolean, default: false
+      add :has_pets, :boolean, default: false
+      add :pet_friendly, :boolean, default: false
+      add :nationality, :string
+      add :preferred_room_type, :string
+      add :birthdate, :date
+
       add :user_id, references(:users, type: :uuid, on_delete: :delete_all), null: false
-      add :preferred_location, :string
-      add :max_price, :decimal
 
       timestamps()
     end
