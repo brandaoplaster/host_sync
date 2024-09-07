0210f689f5c7de56753a1a41a6872e832ff91a4e
brandaoplaster <brandaoplaster@gmail.com>
2024-09-07 19:16:08 -0300

    feat: Updated schema profiles

--- lib/host_sync/accounts/profile.ex
+++ lib/host_sync/accounts/profile.ex
@@ -5,11 +5,20 @@ defmodule HostSync.Accounts.Profile do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
-  @required_params ~w(max_price preferred_location user_id)a
+  @required_params ~w(max_price preferred_room_type user_id nationality birthdate)a
+  @optional_params ~w(bio is_smoker has_pets pet_friendly guest_occupation)a
 
   schema "profiles" do
-    field :preferred_location, :string
-    field :max_price, :decimal
+    field :max_price, :integer
+    field :bio, :string
+    field :preferred_room_type, Ecto.Enum, values: [:single_room, :shared_room, :entire_place]
+    field :is_smoker, :boolean, default: false
+    field :has_pets, :boolean, default: false
+    field :pet_friendly, :boolean, default: false
+    field :guest_occupation, :string
+    field :nationality, :string
+    field :birthdate, :date
+
     belongs_to :user, User
 
     timestamps()
@@ -17,6 +26,8 @@ defmodule HostSync.Accounts.Profile do
 
   def changeset(struct \\ %__MODULE__{}, params) do
     struct
-    |> cast(params, @required_params)
+    |> cast(params, @required_params ++ @optional_params)
+    |> validate_number(:max_price, greater_than: 0)
+    |> validate_inclusion(:preferred_room_type, [:single_room, :shared_room, :entire_place])
   end
 end
