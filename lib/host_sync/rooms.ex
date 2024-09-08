6bee08da8f8bff12e0c127e0c59b705841c661f9
brandaoplaster <brandaoplaster@gmail.com>
2024-09-07 22:15:02 -0300

    feat: Added rooms context

---
+++ lib/host_sync/rooms.ex
@@ -0,0 +1,103 @@
+defmodule HostSync.Rooms do
+  @moduledoc """
+  The Rooms context.
+  """
+
+  import Ecto.Query, warn: false
+  alias HostSync.Repo
+  alias HostSync.Accommodations.Room
+
+  @doc """
+  Returns the list of Rooms.
+
+  ## Examples
+
+      iex> list_rooms()
+      [%Room{}, ...]
+
+  """
+  def list_rooms do
+    Repo.all(Room)
+  end
+
+  @doc """
+  Gets a single Room.
+
+  Raises `Ecto.NoResultsError` if the room does not exist.
+
+  ## Examples
+
+      iex> get_room!(123)
+      %Room{}
+
+      iex> get_room!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_room!(id), do: Repo.get!(Room, id)
+
+  @doc """
+  Creates a Room.
+
+  ## Examples
+
+      iex> create_room(%{field: value})
+      {:ok, %Room{}}
+
+      iex> create_room(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_room(attrs \\ %{}) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a Room.
+
+  ## Examples
+
+      iex> update_room(Room, %{field: new_value})
+      {:ok, %Room{}}
+
+      iex> update_room(Room, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_room(%Room{} = room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Room.
+
+  ## Examples
+
+      iex> delete_room(Room)
+      {:ok, %Room{}}
+
+      iex> delete_room(Room)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_room(%Room{} = room) do
+    Repo.delete(room)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking room changes.
+
+  ## Examples
+
+      iex> change_room(Room)
+      %Ecto.Changeset{data: %Room{}}
+
+  """
+  def change_room(%Room{} = room, attrs \\ %{}) do
+    Room.changeset(room, attrs)
+  end
+end
