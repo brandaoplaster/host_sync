49ac204acd067b69166d0b34730d72c501656afc
brandaoplaster <brandaoplaster@gmail.com>
2024-09-30 23:12:06 -0300

    feat: [WIP] in profile resolver

---
+++ lib/host_sync_web/graphql/resolver/profile_resolver.ex
@@ -0,0 +1,23 @@
+defmodule HostSyncWeb.Graphql.Resolver.ProfileResolver do
+  @moduledoc """
+  Module for resolving profile fields.
+  """
+
+  alias HostSync.Entities.Profile
+
+  def create_profile(_parent, args, _context) do
+    args
+    |> Profile.create()
+    |> handle_create_profile()
+  end
+
+  def get_profile(_parent, %{id: id}, _context) do
+    case Profile.get(id) do
+      nil -> {:error, nil}
+      profile -> {:ok, profile}
+    end
+  end
+
+  defp handle_create_profile({:ok, profile}), do: {:ok, profile}
+  defp handle_create_profile({:error, changeset}), do: {:error, changeset}
+end
