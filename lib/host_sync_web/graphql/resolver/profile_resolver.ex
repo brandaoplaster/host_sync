1ea9d660091c105b2977c4339176bfaf859bf593
brandaoplaster <brandaoplaster@gmail.com>
2024-10-02 21:38:28 -0300

    feat: Added update profile in resolver

--- lib/host_sync_web/graphql/resolver/profile_resolver.ex
+++ lib/host_sync_web/graphql/resolver/profile_resolver.ex
@@ -11,6 +11,13 @@ defmodule HostSyncWeb.Graphql.Resolver.ProfileResolver do
     |> handle_create_profile()
   end
 
+  def update_profile(_parent, %{id: id} = args, _context) do
+    id
+    |> Profile.get()
+    |> Profile.update(args)
+    |> handle_update_profile()
+  end
+
   def get_profile(_parent, %{id: id}, _context) do
     case Profile.get(id) do
       nil -> {:error, nil}
@@ -20,4 +27,7 @@ defmodule HostSyncWeb.Graphql.Resolver.ProfileResolver do
 
   defp handle_create_profile({:ok, profile}), do: {:ok, profile}
   defp handle_create_profile({:error, changeset}), do: {:error, changeset}
+
+  defp handle_update_profile({:ok, profile}), do: {:ok, profile}
+  defp handle_update_profile({:error, changeset}), do: {:error, changeset}
 end
