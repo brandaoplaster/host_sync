c3b4067a1f08614d4e608fcb470b698ec5f99ada
brandaoplaster <brandaoplaster@gmail.com>
2024-10-03 21:50:44 -0300

    feat: Added amenity mutations

--- lib/host_sync_web/graphql/schema/amenity_schema.ex
+++ lib/host_sync_web/graphql/schema/amenity_schema.ex
@@ -12,4 +12,24 @@ defmodule HostSyncWeb.Graphql.Schema.AmenitySchema do
     field :created_at, non_null(:naive_datetime)
     field :updated_at, non_null(:naive_datetime)
   end
+
+  input_object :amenity_input do
+    field :name, non_null(:string)
+    field :description, :string
+  end
+
+  object :amenity_mutations do
+    field :create_amenity, type: :amenity do
+      arg :amenity, non_null(:amenity_input)
+
+      resolve(&HostSyncWeb.Graphql.Resolvers.AmenityResolver.create_amenity/3)
+    end
+
+    field :update_amenity, type: :amenity do
+      arg :id, non_null(:id)
+      arg :amenity, non_null(:amenity_input)
+
+      resolve(&HostSyncWeb.Graphql.Resolvers.AmenityResolver.update_amenity/3)
+    end
+  end
 end
