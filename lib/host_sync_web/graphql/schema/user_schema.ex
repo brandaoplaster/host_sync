3d25bd9a121da72e7db279d8002cf7dd88585977
brandaoplaster <brandaoplaster@gmail.com>
2024-10-05 18:51:43 -0300

    feat: Updated user schema

--- lib/host_sync_web/graphql/schema/user_schema.ex
+++ lib/host_sync_web/graphql/schema/user_schema.ex
@@ -7,13 +7,36 @@ defmodule HostSyncWeb.Graphql.Schema.UserSchema do
   use Absinthe.Schema.Notation
   use Absinthe.Relay.Schema.Notation, :classic
 
+  alias HostSyncWeb.Graphql.Resolvers.UserResolver
+
   object :user do
     field :id, :id
     field :name, :string
     field :email, :string
 
-    field :inserted_at, :datetime
-    field :updated_at, :datetime
+    field :created_at, :naive_datetime
+    field :updated_at, :naive_datetime
+  end
+
+  input_object :user_input do
+    field :email, non_null(:string)
+    field :name, non_null(:string)
+    field :password, non_null(:string)
+  end
+
+  object :user_mutations do
+    field :create_user, :user do
+      arg :input, non_null(:user_input)
+
+      resolve(&UserResolver.create_user/3)
+    end
+
+    field :update_user, :user do
+      arg :id, non_null(:id)
+      arg :input, non_null(:user_input)
+
+      resolve(&UserResolver.update_user/3)
+    end
   end
 
   object :session do
